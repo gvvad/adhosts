04ca6fe5274b62d9980af3cc12ac35deabfaad0a
gvvad <gvvvad92@gmail.com>
2018-06-26 23:50:05 +0300

    comm

--- adhosts.sh
+++ adhosts.sh
@@ -5,6 +5,8 @@ FILE_URL="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 FULLPATH="$(pwd)/$0"
 DIR="$(echo $FULLPATH | sed 's@\(.*\)/.*@\1@')"
 
+# download and prepare hosts file
+# and restart dnsmasq daemon
 update() {
     rm hosts
     wget $FILE_URL --no-check-certificate -O hosts
@@ -13,14 +15,16 @@ update() {
     /etc/init.d/dnsmasq restart
 }
 
+# add ip addres to br-lan and start http server
 start_http() {
     ip address add $BIND_IP dev br-lan
     sed -e "s@{www}@$DIR/www@;s@{pem}@$DIR/keyca.pem@;s@{ca}@$DIR/ca.crt@;s@{pidf}@$DIR/lighttpd.pid@;s@{ip}@$BIND_IP@" "$DIR/lighttpd.conf.pattern" > "$DIR/lighttpd.conf"
     $HTTP_DAEMON -f "$DIR/lighttpd.conf"&
 }
 
+# stop http server
 stop_http() {
-    if [ -f lighttpd.pid ]
+    if [ -f "$DIR/lighttpd.pid" ]
     then
         pid=$(cat "$DIR/lighttpd.pid")
         kill -9 $pid
@@ -37,7 +41,7 @@ if [ "$1" = "start" ]
 then
     stop_http
     start_http
-    if [ ! -f "hosts" ]
+    if [ ! -f "$DIR/hosts" ]
     then
         update
     fi
